8aff295b4740df4e2e2eed60618265e3e59b1e46
drastik <jwjoshuawalker@gmail.com>
2013-07-04 03:19:23 -0500

    Handle nested menus. Unlock menu width. Remove bottom border on last li. Add option to ignore on admin/ pages. Other slight visual improvments.

--- js/responsive_menus_simple.js
+++ js/responsive_menus_simple.js
@@ -14,12 +14,27 @@
       if (!settings.responsive_menus.media_size.length) {
         settings.responsive_menus.media_size = 768;
       }
+      var toggler_class = '';
       // Iterate through our selectors.
       $.each(settings.responsive_menus.css_selectors, function(index, value) {
-        var toggler_class = 'responsive-menus-' + index;
-        $(value).addClass("responsive-menus-simple").wrap('<div class="responsive-menus ' + toggler_class + '" />');
-        $('.' + toggler_class).prepend('<span class="toggler">&#9776;</span>');
+        if ($(value).length > 1) {
+          // Handle nested menus.  Make sure we get the first, but not children.
+          $(value).each(function(val_index) {
+            if (!$(this).parents('ul').length) {
+              toggler_class = 'responsive-menus-' + index + '-' + val_index;
+              $(this).addClass('responsive-menus-simple').wrap('<div class="responsive-menus ' + toggler_class + '" />');
+              $('.' + toggler_class).prepend('<span class="toggler">&#9776;</span>');
+            }
+          });
+        }
+        else {
+          // Single level menus.
+          toggler_class = 'responsive-menus-' + index;
+          $(value).addClass('responsive-menus-simple').wrap('<div class="responsive-menus ' + toggler_class + '" />');
+          $('.' + toggler_class).prepend('<span class="toggler">&#9776;</span>');
+        }
       });
+
       // Adjustable width instead of @media queries.
       if (window.innerWidth < settings.responsive_menus.media_size) {
         $('.responsive-menus').addClass('responsified');
@@ -32,7 +47,6 @@
       $(window).resize(function() {
         if(window.innerWidth > settings.responsive_menus.media_size) {
           $('.responsive-menus').removeClass('responsified');
-          $('.responsive-menus-simple').removeClass('responsive-toggled');
         }
         if (window.innerWidth < settings.responsive_menus.media_size) {
           $('.responsive-menus').addClass('responsified');
