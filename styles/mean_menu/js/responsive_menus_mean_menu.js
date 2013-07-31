1316b947ef8382773c95a148d91d22f0f2b57b00
drastik <jwjoshuawalker@gmail.com>
2013-07-31 04:15:53 -0400

    Added MeanMenu style! Moved Simple style into new styles directory along with Mean Menu.

---
+++ styles/mean_menu/js/responsive_menus_mean_menu.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Integrate Mean Menu library with Responsive Menus module.
+ */
+(function ($) {
+  Drupal.behaviors.responsive_menus_mean_menu = {
+    attach: function (context, settings) {
+      settings.responsive_menus = settings.responsive_menus || {};
+      if (!settings.responsive_menus.selectors.length) {
+        return;
+      }
+      // Call meanmenu() with our custom settings.
+      $(settings.responsive_menus.selectors).meanmenu({
+        meanMenuClose: settings.responsive_menus.close_txt || "X",
+        meanMenuCloseSize: settings.responsive_menus.close_size || "18px",
+        meanMenuOpen: settings.responsive_menus.trigger_txt || "<span /><span /><span />",
+        meanRevealPosition: settings.responsive_menus.position || "right",
+        meanScreenWidth: settings.responsive_menus.media_size || "480",
+        meanShowChildren: settings.responsive_menus.show_children || true,
+        meanExpandableChildren: settings.responsive_menus.expand_children || true,
+        meanExpand: settings.responsive_menus.expand_txt || "+",
+        meanContract: settings.responsive_menus.contract_txt || "-",
+        meanRemoveAttrs: settings.responsive_menus.remove_attrs || true
+      });
+
+    }
+  };
+}(jQuery));
