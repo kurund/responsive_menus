1f8fcbf38d5166cd41905be206efd326dd6781f4
pjcdawkins <pjcdawkins@1025236.no-reply.drupal.org>
2013-08-06 20:12:33 -0500

    Issue #2059081 by pjcdawkins: Fixed AJAX calls sometimes duplicate the menu (Mean Menus).

--- styles/mean_menu/js/responsive_menus_mean_menu.js
+++ styles/mean_menu/js/responsive_menus_mean_menu.js
@@ -10,17 +10,19 @@
         return;
       }
       // Call meanmenu() with our custom settings.
-      $(settings.responsive_menus.selectors).meanmenu({
-        meanMenuClose: settings.responsive_menus.close_txt || "X",
-        meanMenuCloseSize: settings.responsive_menus.close_size || "18px",
-        meanMenuOpen: settings.responsive_menus.trigger_txt || "<span /><span /><span />",
-        meanRevealPosition: settings.responsive_menus.position || "right",
-        meanScreenWidth: settings.responsive_menus.media_size || "480",
-        meanShowChildren: settings.responsive_menus.show_children || true,
-        meanExpandableChildren: settings.responsive_menus.expand_children || true,
-        meanExpand: settings.responsive_menus.expand_txt || "+",
-        meanContract: settings.responsive_menus.contract_txt || "-",
-        meanRemoveAttrs: settings.responsive_menus.remove_attrs || true
+      $(settings.responsive_menus.selectors).once('responsive-menus-mean-menu', function() {
+        $(this).meanmenu({
+          meanMenuClose: settings.responsive_menus.close_txt || "X",
+          meanMenuCloseSize: settings.responsive_menus.close_size || "18px",
+          meanMenuOpen: settings.responsive_menus.trigger_txt || "<span /><span /><span />",
+          meanRevealPosition: settings.responsive_menus.position || "right",
+          meanScreenWidth: settings.responsive_menus.media_size || "480",
+          meanShowChildren: settings.responsive_menus.show_children || true,
+          meanExpandableChildren: settings.responsive_menus.expand_children || true,
+          meanExpand: settings.responsive_menus.expand_txt || "+",
+          meanContract: settings.responsive_menus.contract_txt || "-",
+          meanRemoveAttrs: settings.responsive_menus.remove_attrs || true
+        });
       });
 
     }
