de8556efa71f8d9f21665fe5224e49e8772421c7
drastik <jwjoshuawalker@gmail.com>
2013-08-09 10:15:29 -0400

    Context module integration! Ability to have multiple rule sets defined (using context). Fix window.resize function bound more than once on ajax in simple style.

--- styles/responsive_menus_simple/js/responsive_menus_simple.js
+++ styles/responsive_menus_simple/js/responsive_menus_simple.js
@@ -3,36 +3,38 @@
  * Simple responsification of menus.
  */
 (function ($) {
-
   /**
    * Handle clicks & toggling the menu.
    */
   var toggler_click = function() {
     $(this).parent().toggleClass('responsive-toggled');
   };
-
   // Iterate through selectors, check window sizes, add some classes.
   Drupal.behaviors.responsive_menus = {
     attach: function (context, settings) {
       settings.responsive_menus = settings.responsive_menus || {};
-      if (!settings.responsive_menus.selectors.length) {
+      $('.responsive-menus .toggler').unbind('click');
+      $.each(settings.responsive_menus, function(ind, iteration){
+        if (iteration.responsive_menus_style != 'responsive_menus_simple') {
+          return true;
+        }
+      if (!iteration.selectors.length) {
         return;
       }
-      if (!settings.responsive_menus.media_size.length) {
-        settings.responsive_menus.media_size = 768;
+      if (!iteration.media_size.length) {
+        iteration.media_size = 768;
       }
       // Handle clicks & toggling.
       var toggler_class = '';
-      var toggler_text = settings.responsive_menus.toggler_text;
-      $('.responsive-menus .toggler').unbind('click');
+      var toggler_text = iteration.toggler_text;
       // Iterate through our selectors.
-      $.each(settings.responsive_menus.selectors, function(index, value) {
+      $.each(iteration.selectors, function(index, value) {
         if ($(value).length > 1) {
           // Handle nested menus.  Make sure we get the first, but not children.
           $(value).each(function(val_index) {
             if (!$(this).parents('ul').length) {
               if (!$(this).hasClass('responsive-menus-simple')) {
-                toggler_class = 'responsive-menus-' + index + '-' + val_index;
+                toggler_class = 'responsive-menus-' + ind + '-' + index + '-' + val_index;
                 $(this).addClass('responsive-menus-simple').wrap('<div class="responsive-menus ' + toggler_class + '" />');
                 $('.' + toggler_class).prepend('<span class="toggler">' + toggler_text + '</span>');
               }
@@ -42,7 +44,7 @@
         else {
           // Single level menus.
           if (!$(value).hasClass('responsive-menus-simple')) {
-            toggler_class = 'responsive-menus-' + index;
+            toggler_class = 'responsive-menus-' + ind + '-' + index;
             $(value).addClass('responsive-menus-simple').wrap('<div class="responsive-menus ' + toggler_class + '" />');
             $('.' + toggler_class).prepend('<span class="toggler">' + toggler_text + '</span>');
           }
@@ -51,22 +53,28 @@
       // Re-bind click function.  This handles ajax events.
       $('.responsive-menus .toggler').bind('click', toggler_click);
       // Use absolute positioning.
-      if (settings.responsive_menus.absolute) {
+      if (iteration.absolute) {
         $('.responsive-menus-simple').addClass('absolute');
       }
-      // Adjustable width instead of @media queries.
-      if (window.innerWidth < settings.responsive_menus.media_size) {
-        $('.responsive-menus').addClass('responsified');
-      }
-      // Handle window resizing.
-      $(window).resize(function() {
-        if(window.innerWidth > settings.responsive_menus.media_size) {
-          $('.responsive-menus').removeClass('responsified');
-        }
-        if (window.innerWidth < settings.responsive_menus.media_size) {
+      // Other things to bind once.
+      $('body').once('responsive-menus-load', function(){
+        // Adjustable width instead of @media queries.
+        if (window.innerWidth < iteration.media_size) {
           $('.responsive-menus').addClass('responsified');
         }
+        // Handle window resizing.
+        $(window).resize(function() {
+          if(window.innerWidth > iteration.media_size) {
+            $('.responsive-menus').removeClass('responsified');
+          }
+          if (window.innerWidth < iteration.media_size) {
+            $('.responsive-menus').addClass('responsified');
+          }
+        });
       });
+
+});
+
     }
   };
 
